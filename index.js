174fda90301a0311c1f65eb749d3250782647ce6
meesam4687 <meesam4687@gmail.com>
2024-06-12 21:53:57 +0530

    Include Server

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 require('dotenv').config();
 const fetch = require("node-fetch-commonjs");
+const express = require('express');
+
 
 const client = new Discord.Client({
   intents: [
@@ -47,5 +49,13 @@ client.on('messageCreate', (message) => {
     if(message.channel.id !== "1153958075548573756") return;
     send(message.content)
 })
+
+let app = express();
+app.get("/", (req, res) => {
+    res.send("Running");
+});
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
 
 client.login(process.env.TOKEN)
