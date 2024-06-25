1ffea7d8208af4f5fefe70159f5a979fba175487
Meesam <meesam4687@gmail.com>
2024-06-25 16:30:25 +0000

    message filter

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const express = require('express');
 const send = require('./webhook.js');
 
 const client = new Discord.Client({
-  intents: [
-    Discord.GatewayIntentBits.Guilds,
-    Discord.GatewayIntentBits.GuildVoiceStates,
-    Discord.GatewayIntentBits.GuildMessages,
-    Discord.GatewayIntentBits.MessageContent
-  ]
+    intents: [
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildVoiceStates,
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.MessageContent
+    ]
 });
 
 const webhooks = [
@@ -21,8 +21,23 @@ client.on('ready', () => {
 });
 
 client.on('messageCreate', (message) => {
-    if(message.channel.id !== "1153958075548573756") return;
+    if (message.channel.id !== "1153958075548573756") return;
     send(message.content.replace(/(<@(.+)>)/g, ""), webhooks)
+    fetch("https://api.llama-api.com/chat/completions", {
+        method: 'post',
+        body: JSON.stringify({
+            "model": "llama3-70b",
+            "messages": [
+                { "role": "user", "content": "I want you to help me moderate a discord server for cybercrew. I want the chat to be clean, not offensive and always be to the point. No random nonsense or people playing around. I will be sending you a message, you have to check if that message is allowed or not. If the message is allowed then reply with OK if not the reply with NOK. ONLY REPLY WITH OK OR NOK, NOTHING ELSE" },
+                { "role": "user", "content": message.content }
+            ]
+        }),
+        headers: { 'Authorization': `Bearer ${process.env.LLAMA}`, 'Content-Type': 'application/json' }
+    }).then(response => response.json()).then(data => {
+        if (data.choices[0].message.content == 'NOK') {
+            message.delete()
+        }
+    })
 })
 
 let app = express();
@@ -32,5 +47,7 @@ app.get("/", (req, res) => {
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
+
+
 
 client.login(process.env.TOKEN)
